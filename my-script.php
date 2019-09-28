2f7e471a739141bb19dddd9659ac749e32614b03
Rich Samartino <rich@richsamartino.com>
2019-09-28 00:31:50 -0700

    calculate total with discount qty

--- my-script.php
+++ my-script.php
@@ -9,7 +9,38 @@ $priceSetter->setPricing('B', 12);
 $priceSetter->setPricing('C', 1.25, 6, 6);
 $priceSetter->setPricing('D', .15);
 
-$priceScanner = new \Rich\PriceScanner(time());
+
+$priceScanner = new \Rich\PriceScanner(microtime());
+
+$priceScanner->scan('A');
+$priceScanner->scan('B');
+$priceScanner->scan('C');
+$priceScanner->scan('D');
+$priceScanner->scan('A');
+$priceScanner->scan('B');
+$priceScanner->scan('A');
+$priceScanner->scan('A');
+
+$result = $priceScanner->total();
+
+echo $result . PHP_EOL;
+
+$priceScanner = new \Rich\PriceScanner(microtime());
+
+$priceScanner->scan('C');
+$priceScanner->scan('C');
+$priceScanner->scan('C');
+$priceScanner->scan('C');
+$priceScanner->scan('C');
+$priceScanner->scan('C');
+$priceScanner->scan('C');
+
+$result = $priceScanner->total();
+
+echo $result . PHP_EOL;
+
+
+$priceScanner = new \Rich\PriceScanner(microtime());
 
 $priceScanner->scan('A');
 $priceScanner->scan('B');
--- src/PriceScanner.php
+++ src/PriceScanner.php
@@ -11,13 +11,14 @@ class PriceScanner
 
     private $cart;
 
-    public function __construct(int $timestamp)
+    public function __construct(float $timestamp)
     {
         $this->timestamp = $timestamp;
         $this->database = new \Filebase\Database([
             'dir' => __DIR__ . '/database'
         ]);
         $this->cart = $this->database->get($this->timestamp);
+        $this->cart->save();
     }
 
     public function scan(string $code)
@@ -29,9 +30,25 @@ class PriceScanner
     public function total()
     {
         $total = 0;
-        foreach ($this->cart->items as $item) {
-            $total += $this->database->get($item)->price;
+
+//        print_r(array_count_values($this->cart->items));
+
+        foreach (array_count_values($this->cart->items) as $code => $count) {
+            $priceData = $this->database->get($code);
+            if ($priceData->discountQty && ($count <=> $priceData->discountQty) >= 0) {
+                echo "$code is greater" . PHP_EOL . ($count - $priceData->discountQty) . PHP_EOL . PHP_EOL;
+                $total += $priceData->discountPrice + (($count - $priceData->discountQty) * $priceData->price);
+                echo 'total: ' . $total . PHP_EOL;
+            } else {
+                echo "$code is not greater" . PHP_EOL;
+                $total += $this->database->get($code)->price * $count;
+            }
         }
+
+//        foreach ($this->cart->items as $item) {
+//        }
+
+
 
         echo $total . PHP_EOL;
     }
